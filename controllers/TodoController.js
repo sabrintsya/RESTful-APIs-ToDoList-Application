505c5c9e90651739d8ab743d8e0fac9808bc8888
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-17 14:44:50 +0700

    add save todo

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -4,3 +4,15 @@ module.exports.getTodo = async (req, res) => {
     const Todo = await TodoModel.find()
     res.send(Todo)
 }
+
+module.exports.saveTodo =  async (req, res) => {
+    const { text } = req.body
+
+    TodoModel
+    .creat({text})
+    .then((Data) => {
+        console.log("Add Success!");
+        console.log(Data);
+        res.send(Data)
+    })
+}
