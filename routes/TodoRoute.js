e58ac968fba71478d38502f36c93d8d8715bce79
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-17 15:47:26 +0700

    membenarkan code yang error

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,8 +1,8 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const { getTodo, saveTodo } = require("../controllers/TodoController");
-const router = Router()
+const router = Router();
 
-router.get('/', getTodo)
-router.post('/save', saveTodo)
+router.get('/', getTodo); // This will handle GET requests at the root of this route
+router.post('/save', saveTodo); // This will handle POST requests to /save
 
 module.exports = router;
