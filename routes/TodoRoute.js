0d2e87ae576a0073a5efcecafae23652069ac166
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-17 15:03:33 +0700

    toute get and post todo

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,7 +1,8 @@
 const {Router} = require("express");
-const { getTodo } = require("../controllers/TodoController");
+const { getTodo, saveTodo } = require("../controllers/TodoController");
 const router = Router()
 
 router.get('/', getTodo)
+router.post('/save', saveTodo)
 
 module.exports = router;
