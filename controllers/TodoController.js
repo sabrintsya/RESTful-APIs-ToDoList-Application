6754e69d4b989ea0fb2f745979a72194c50b55fb
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-17 15:45:50 +0700

    memperbaiki code yang error

--- controllers/TodoController.js
+++ controllers/TodoController.js
@@ -1,18 +1,22 @@
-const TodoModel = require('../models/TodoModel')
+const TodoModel = require('../models/TodoModel');
 
 module.exports.getTodo = async (req, res) => {
-    const Todo = await TodoModel.find()
-    res.send(Todo)
-}
+    const Todo = await TodoModel.find();
+    res.send(Todo);
+};
 
-module.exports.saveTodo =  async (req, res) => {
-    const { text } = req.body
+module.exports.saveTodo = async (req, res) => {
+    const { text } = req.body;
 
     TodoModel
-    .creat({text})
-    .then((Data) => {
-        console.log("Add Success!");
-        console.log(Data);
-        res.send(Data)
-    })
-}
+        .create({ text })  // Corrected from creat to create
+        .then((Data) => {
+            console.log("Add Success!");
+            console.log(Data);
+            res.send(Data);
+        })
+        .catch(err => {
+            console.error("Error saving todo:", err);
+            res.status(500).send("Error saving todo");
+        });
+};
