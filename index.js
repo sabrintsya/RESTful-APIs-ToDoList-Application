f7dd8f778f2efdf7e7d70388f6f875e5b4ae75af
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-18 11:02:03 +0700

    change port 5001

--- index.js
+++ index.js
@@ -6,20 +6,16 @@ const routes = require('./routes/TodoRoute');
 dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI,   
-  {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
- })
- .then(() => {
-   console.log('Connected to MongoDB');
- })
- .catch(err => {
-   console.error('Error connecting to MongoDB:', err);
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch(err => {
+  console.error('Error connecting to MongoDB:', err);
 });
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 
