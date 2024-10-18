0d46a7ae47367c3f77e5ea4e933eb0b30ee9a9de
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-18 11:01:06 +0700

    add update and delete todo

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { getTodo, saveTodo } = require("../controllers/TodoController");
+const { getTodo, saveTodo, updateTodo, deleteTodo } = require("../controllers/TodoController");
 const router = Router();
 
 router.get('/', getTodo); // This will handle GET requests at the root of this route
 router.post('/save', saveTodo); // This will handle POST requests to /save
+router.post('/update', updateTodo); // This will handle POST requests to /update
+router.post('/delete', deleteTodo);// This will handle POST requests to /delete
 
 module.exports = router;
