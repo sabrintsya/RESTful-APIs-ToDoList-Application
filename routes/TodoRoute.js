9314f8089629d5409c4a177f7fc381f143de4d54
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-19 10:15:43 +0700

    add todo

--- routes/TodoRoute.js
+++ routes/TodoRoute.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
-const { getTodo, saveTodo, updateTodo, deleteTodo } = require("../controllers/TodoController");
+const { validateToken, checkRole } = require("../middleware/AuthMiddleware");
+
+const { getTodo, saveTodo, updateTodo, deleteTodo,deleteAllTodo, getAllTodo, createTodo} = require("../controllers/TodoController");
+const { login, register } = require("../controllers/AuthController");
+
 const router = Router();
 
 router.get('/', getTodo); // This will handle GET requests at the root of this route
-router.post('/save', saveTodo); // This will handle POST requests to /save
-router.post('/update', updateTodo); // This will handle POST requests to /update
-router.post('/delete', deleteTodo);// This will handle POST requests to /delete
+router.get('/', getAllTodo);
+router.post("/regis", register);
+router.post("/login", login);
+router.post('/createTodo', validateToken, createTodo);
+router.post('/save', validateToken , saveTodo); // This will handle POST requests to /save
+router.post('/update', validateToken, updateTodo); // This will handle POST requests to /update
+router.delete('/', validateToken, checkRole, deleteAllTodo);
+router.delete('/:id', validateToken, checkRole, deleteTodo);// This will handle POST requests to /delete
 
 module.exports = router;
