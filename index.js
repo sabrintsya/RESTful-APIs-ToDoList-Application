9ebf09fe91724e352e8e780a293237a4eb16ac61
sabrina natasya bilbina <sabrinatasya170306@gmail.com>
2024-10-23 21:07:27 +0700

    change index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL;
-console.log(MONGODB_URL);
-const db = mongoose.connect(MONGODB_URL);
-
 const allRoutes = require("./routes/app");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-db.then(() => {
-  console.log("berhasil connect ke server");
-}).catch(() => {
-  console.log("gagal connect ke server");
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`connected to MongooDB... `))
+.catch((err) => console.log(err))
+
+app.listen(PORT, () => {
+  console.log("server running on PORT " + 3001);
 });
-require('dotenv').config();
-
 
 app.use(express.json());
 app.use(allRoutes);
 
-app.listen(PORT, () => {
-  console.log("server running on PORT " + 3000);
-});
 
-module.exports = db;
